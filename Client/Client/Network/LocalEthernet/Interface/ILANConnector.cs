ac8d2331c34c7a35186b80d07928e9123c4f788a
正正 <69309768+HitteField@users.noreply.github.com>
2022-03-03 19:49:00 +0800

    做出了一些修改
    增加了LAN管理器
    重排了文件结构
    将框架改为net46

---
+++ Client/Client/Network/LocalEthernet/Interface/ILANConnector.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LittleSheep
+{
+    /// <summary>
+    /// 局域网连接模块
+    /// </summary>
+    interface ILANConnector
+    {
+        /// <summary>
+        /// 目标设备套接字
+        /// </summary>
+        Socket Clientfd
+        {
+            get;
+        }
+        
+        /// <summary>
+        /// 初始化LAN连接模块
+        /// </summary>
+        /// <returns>初始化结果</returns>
+        bool Initialization();
+        /// <summary>
+        /// 广播一条消息
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <returns>广播成功与否</returns>
+        bool BoradcastMsg(MsgBase msg);
+        /// <summary>
+        /// 发送一条消息
+        /// </summary>
+        /// <param name="socket"></param>
+        /// <param name="msg"></param>
+        /// <returns>发送成功与否</returns>
+        bool SendMsg(Socket socket, MsgBase msg);
+        /// <summary>
+        /// 开始接收报文
+        /// </summary>
+        void StartReceive(int port);
+        /// <summary>
+        /// 关闭接收报文
+        /// </summary>
+        void HaltReceive();
+    }
+}
